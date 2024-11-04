9030ad26b24ccb4ebe3027c9aa27807d0b594e14
saketsn <saketnandan15@gmail.com>
2024-11-04 22:20:14 +0530

    update

--- bobbleSort.c
+++ bobbleSort.c
@@ -16,8 +16,11 @@ void printArray(int *A , int n)
 void bubblesort(int *A , int n)
 {
     int temp;
+    int isSorted = 0;
     for(int i = 0; i < n; i++)  // for number of passes
     {
+        printf("Working on pass number %d \n", i+1);
+        isSorted = 1;
         for(int j = 0; j < n-1-i; j++) // for comparison in each pass 
         {
             
@@ -26,15 +29,22 @@ void bubblesort(int *A , int n)
              temp = A[j];
              A[j] = A[j + 1];
              A[j + 1] = temp;
+             isSorted =0;
             }
         }
+         if(isSorted)
+          {
+            return ;
+          }
     }
+   
 }
 
 int main()
 {
 
 int A[] = { 12 , 54, 65, 7, 23, 9};
+//int A[] = { 1 , 2, 3, 4, 5, 6};
 int n = 6;
 printArray(A , n);
 
